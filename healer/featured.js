5615e70a48b2300d63c455cd23cc133983f8d572
HEALER <65026164+healer-op@users.noreply.github.com>
2024-09-28 21:06:02 +0530

    oksy

--- healer/featured.js
+++ healer/featured.js
@@ -1,4 +1,4 @@
-let feature = "https://script.google.com/macros/s/AKfycbzD8GcgXkv9CzyjSIduuaakwFOZWkU-V4d6iw7UFvl25QmHxeL_MQnxOC7N5pEQKkIr/exec"
+let feature = "https://script.google.com/macros/s/AKfycbw1uZq2LRy6YrlmKI8xPWS39180cJC8oZWTAP2ieIvP4mjOLElyEZYIAs0X_nVbpnM/exec"
 
 
 fetch(`${feature}`)
@@ -9,18 +9,21 @@ fetch(`${feature}`)
         return response.json();
     })
     .then(data => {
-        
+        // console.log(data)
         let featured = data.filter(o => o.Type === 'featured');
-        let katana = data.filter(o => o.Type === 'katana');
-        let skins = data.filter(o => o.Type === 'wraps');
+        let oversize = data.filter(o => o.Type === 'oversize');
+        let baggie = data.filter(o => o.Type === 'baggy');
+        let hoodie = data.filter(o => o.Type === 'hoodie');
         // console.log(featured)
         // console.log(data[0]);
-
+        featured = featured.slice(0,8)
+        baggie = baggie.slice(0,4)
+        // console.log(baggie)
         //featured
         const featured_html = featured.map((f, i) => {
             return `
         <div class="product text-center col-lg-3 col-md-6 col-12">
-            <img class="img-fluid mb-3" src="${featured[i].Image}" width="217px" heigth="217px" alt="">
+            <img src="${featured[i].Image}" width="auto" height="217px" alt="">
             <div class="star">
                 <i class='bx bxs-star'></i>
                 <i class='bx bxs-star'></i>
@@ -37,11 +40,11 @@ fetch(`${feature}`)
         }).join('');
         document.querySelector("#featuredl").insertAdjacentHTML("afterbegin", featured_html);
 
-        //katana
-        const katana_html = katana.map((f, i) => {
+        //baggie
+        const baggy_html = baggie.map((f, i) => {
             return `
             <div class="product text-center col-lg-3 col-md-6 col-12">
-            <img class="img-fluid mb-3" src="${katana[i].Image}" alt="">
+            <img  src="${baggie[i].Image}" width="auto" height="217px" alt="">
             <div class="star">
               <i class='bx bxs-star'></i>
               <i class='bx bxs-star'></i>
@@ -49,37 +52,37 @@ fetch(`${feature}`)
               <i class='bx bxs-star'></i>
               <i class='bx bxs-star'></i>
             </div>
-            <h5 class="p-name">${katana[i].Name}</h5>
-            <h4 class="p-price">₹${katana[i].Price}</h4>
-            <h4 class="p-price" style="display: none;">${katana[i].Tags}</h4>
+            <h5 class="p-name">${baggie[i].Name}</h5>
+            <h4 class="p-price">₹${baggie[i].Price}</h4>
+            <h4 class="p-price" style="display: none;">${baggie[i].Tags}</h4>
             <a href="shop.html"><button class="buy-button">Shop Now</button></a>
     
           </div>
 
         </div>`;
         }).join('');
-        document.querySelector("#katanas").insertAdjacentHTML("afterbegin", katana_html);
+        document.querySelector("#baggy").insertAdjacentHTML("afterbegin", baggy_html);
 
-        //skins
-        const skins_html = skins.map((f, i) => {
-            return `
-            <div class="product text-center col-lg-3 col-md-6 col-12">
-            <img class="img-fluid mb-3" src="${skins[i].Image}" alt="">
-            <div class="star">
-              <i class='bx bxs-star'></i>
-              <i class='bx bxs-star'></i>
-              <i class='bx bxs-star'></i>
-              <i class='bx bxs-star'></i>
-              <i class='bx bxs-star'></i>
-            </div>
-            <h5 class="p-name">${skins[i].Name}</h5>
-            <h4 class="p-price">₹${skins[i].Price}</h4>
-            <h4 class="p-price" style="display: none;">${skins[i].Tags}</h4>
-            <a href="shop.html"><button class="buy-button">Shop Now</button></a>
+        // //skins
+        // const skins_html = skins.map((f, i) => {
+        //     return `
+        //     <div class="product text-center col-lg-3 col-md-6 col-12">
+        //     <img class="img-fluid mb-3" src="${skins[i].Image}" alt="">
+        //     <div class="star">
+        //       <i class='bx bxs-star'></i>
+        //       <i class='bx bxs-star'></i>
+        //       <i class='bx bxs-star'></i>
+        //       <i class='bx bxs-star'></i>
+        //       <i class='bx bxs-star'></i>
+        //     </div>
+        //     <h5 class="p-name">${skins[i].Name}</h5>
+        //     <h4 class="p-price">₹${skins[i].Price}</h4>
+        //     <h4 class="p-price" style="display: none;">${skins[i].Tags}</h4>
+        //     <a href="shop.html"><button class="buy-button">Shop Now</button></a>
     
-          </div>
+        //   </div>
 
-        </div>`;
-        }).join('');
-        document.querySelector("#skins").insertAdjacentHTML("afterbegin", skins_html);
+        // </div>`;
+        // }).join('');
+        // document.querySelector("#skins").insertAdjacentHTML("afterbegin", skins_html);
     })
