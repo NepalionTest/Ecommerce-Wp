fb732cfba5bb279208de7d080a2400910da1b7c3
HEALER <65026164+healer-op@users.noreply.github.com>
2024-09-29 12:14:56 +0530

    okay

--- cart.js
+++ cart.js
@@ -1,11 +1,19 @@
+const params = new URLSearchParams(document.location.search);
+let filter = params.get("filter");
+let product = params.get("product");
+
+
+let feature = "https://script.google.com/macros/s/AKfycbw1uZq2LRy6YrlmKI8xPWS39180cJC8oZWTAP2ieIvP4mjOLElyEZYIAs0X_nVbpnM/exec"
+
 const cartContainer = document.querySelector('.cart-container');
 
 const productList = document.querySelector('.product-list');
 const cartList = document.querySelector('.cart-list');
 const cartTotalValue = document.getElementById('cart-total-value');
-let feature = "https://script.google.com/macros/s/AKfycbxa6iiG3ohmI6ZaEmBcp8vGzRmM61MN542Lpe0fmnVjIise9VTrBQECY8oPjlk-RGSZ/exec"
+
 let cartItemID = 0;
-let phoneNumber = `8437793637`
+let phoneNumber = `8437793637` //your company whatsapp number
+
 
 eventListeners();
 
@@ -14,7 +22,7 @@ function eventListeners() {
     window.addEventListener('DOMContentLoaded', () => {
         loadJSON();
     });
-    productList.addEventListener('click' , purchaseProduct);
+    productList.addEventListener('click', purchaseProduct);
 }
 // show & hide Cart
 
@@ -23,30 +31,42 @@ function toggleHide() {
 
     if (container.style.display != 'none') {
         container.style.display = 'none';
-    }
-    else {
+    } else {
         container.style.display = 'block';
     }
 }
-//Loading products from JSON to our Shop page
-function loadJSON() {
-    let feature = "https://script.google.com/macros/s/AKfycbzD8GcgXkv9CzyjSIduuaakwFOZWkU-V4d6iw7UFvl25QmHxeL_MQnxOC7N5pEQKkIr/exec"
-    fetch(`${feature}`)
-    .then(response => {
-        if (!response.ok) {
-            throw Error("ERROR");
-        }
-        return response.json();
-    })
-    .then(data => {
-        // console.log(featured)
-        // console.log(data[0]);
 
-        //featured
+
+
+async function loadJSON() {
+    try {
+        let data = await (await fetch(`${feature}`)).json();
+        console.log(filter)
+        if (typeof filter === 'undefined' || filter === null) {}
+        if (filter == "oversize") {
+            let oversize = data.filter(o => o.Type === 'oversize');
+            if ((oversize).length != 0) {
+                data = oversize
+            }
+        }
+        if (filter == "baggy") {
+            let baggy = data.filter(o => o.Type === 'baggy');
+            if ((baggy).length != 0) {
+                data = baggy
+            }
+        }
+        if (filter == "hoodie") {
+            let hoodie = data.filter(o => o.Type === 'hoodie');
+            if ((hoodie).length != 0) {
+                data = hoodie
+            }
+        }
+        // console.log(data)
+
         const html = data.map((f, i) => {
             return `
             <div class="product text-center col-lg-3 col-md-6 col-12">
-            <img class="img-fluid mb-3 p-img" src="${data[i].Image}" alt="">
+            <img class="ccimgh" src="${data[i].Image}" width="auto" height="217px" alt="">
             <div class="star">
               <i class='bx bxs-star'></i>
               <i class='bx bxs-star'></i>
@@ -58,122 +78,151 @@ function loadJSON() {
             <h4 class="p-price">₹${data[i].Price}</h4>
             <h4 class="p-price" style="display: none;">${data[i].Tags}</h4>
             <button class="buy-button" onclick ="msg()">Add to Cart</button>
-        
-          </div>`;
+            </div>`;
         }).join('');
         document.querySelector("#all").insertAdjacentHTML("afterbegin", html);
-        // document.getElementById('all').innerHTML = html;
-    })
+
+    } catch (error) {
+
+    }
 }
+
 //cart hide/show and show cart items
 
-function toggleHide(){
-    let cart= document.getElementById('cart');
+function toggleHide() {
+    let cart = document.getElementById('cart');
 
-    if(cart.style.display != 'none' ){
+    if (cart.style.display != 'none') {
         cart.style.display = 'none';
-    }
-    else{
+    } else {
         cart.style.display = 'block';
     }
     //To show items in cart after clicking on bag icon
-       for(i=0; i<itemArray.length; i++)
-        {
-           document.getElementById('cart-list').innerHTML = itemArray[i];
-        }
-        
-       /* for(i=0; i<itemArray.length; i++)
-        {
-           localStorage.setItem("array" , itemArray[i]);
-        }
-        for(i=0; i<itemArray.length; i++)
-        {
-           document.getElementById('cart-list').innerHTML = localStorage.getItem("array");
-        }
-        */
+    for (i = 0; i < itemArray.length; i++) {
+        document.getElementById('cart-list').innerHTML = itemArray[i];
+    }
 
-        window.scrollTo(0, 0); 
+
+    window.scrollTo(0, 0);
 }
 // Add to cart 
 
-function purchaseProduct(e){
-    if(e.target.classList.contains('buy-button')){
+function purchaseProduct(e) {
+    if (e.target.classList.contains('buy-button')) {
         let product = e.target.parentElement;
-        
+
         getProductInfo(product);
     }
-    cartItemID ++;
-    
+    cartItemID++;
+
 }
 
- function getProductInfo(product){
+function getProductInfo(product) {
     let productInfo = {
-        id :cartItemID,
-        imgsrc : product.querySelector('.p-img').src,
-        name : product.querySelector('.p-name').textContent,
-        price :product.querySelector('.p-price').textContent,
+        id: cartItemID,
+        imgsrc: product.querySelector('.ccimgh').src,
+        name: product.querySelector('.p-name').textContent,
+        price: product.querySelector('.p-price').textContent,
     }
 
-      addtocart(productInfo);
- }
+    addtocart(productInfo);
+}
 
- let item='';
- let itemArray = new Array();
- let cartItems = new Array();
- let itemPrice = 0
- 
- function addtocart(productInfo){    
-         item += `<tr>
+let item = '';
+let itemArray = new Array();
+let cartItems = new Array();
+let itemPrice = 0
+
+function addtocart(productInfo) {
+    item += `<tr>
             <td><a href="#"><i class='bx bxs-trash-alt'></i></a></td>
             <td><img src="${productInfo.imgsrc}" alt="Loading"></td>
             <td><h5 class="cart-item-name"> ${productInfo.name}</h5></td>
             <td><h5 class="cart-item-price"> ${productInfo.price} </h5></td>
           </tr>`;
-          itemPrice += parseInt((productInfo.price).replaceAll("₹",""));
-   //storing sleceted item in array
+    itemPrice += parseInt((productInfo.price).replaceAll("₹", ""));
+    //storing sleceted item in array
 
-        itemArray[cartItemID] = item;
-        cartItems[cartItemID] = productInfo.name;
-            document.getElementById('count').innerHTML = itemArray.length;
-            if(itemArray.length>1){
-            document.getElementById('item-count').innerHTML =`Total-Price: ₹ ${itemPrice}`; 
-            document.getElementById('checkout').innerHTML = `<button id="checkout" onclick="whatsapp(cartItems,itemPrice)">CheckOut</button>`;
-            }
+    itemArray[cartItemID] = item;
+    cartItems[cartItemID] = productInfo.name;
+    document.getElementById('count').innerHTML = itemArray.length;
+    if (itemArray.length > 1) {
+        document.getElementById('item-count').innerHTML = `Total-Price: ₹ ${itemPrice}`;
+        document.getElementById('checkout').innerHTML = `<button id="checkout" onclick="whatsapp(cartItems,itemPrice)">CheckOut</button>`;
+    }
 
- }
- 
- function whatsapp(cartItems,itemPrice){
+}
+
+function whatsapp(cartItems, itemPrice) {
     console.log(cartItems)
     let chatMessage = cartItems.toString();
-    chatMessage = chatMessage.replaceAll("empty","")
-    chatMessage = chatMessage.replaceAll(",","%0A")
+    chatMessage = chatMessage.replaceAll("empty", "")
+    chatMessage = chatMessage.replaceAll(",", "%0A")
     chatMessage = `⭐Hi Custom Clan i want to order%0A` + chatMessage + `%0A%0A🚚Cart Value ${itemPrice}`
     window.open(`https://api.whatsapp.com/send?phone=${phoneNumber}&text=${chatMessage}`)
- }
-          
-           
-  //To show aknowledgement msg after clicking on add to cart button
-  
-  function msg()
-  {
+}
+
+
+//To show aknowledgement msg after clicking on add to cart button
+
+function msg() {
     let msg = document.getElementById('msg-appear');
-    msg.style.display ='block';
+    msg.style.display = 'block';
     setTimeout(hideElement, 1500);
 
-    function hideElement(){
-          msg.style.display ='none';
+    function hideElement() {
+        msg.style.display = 'none';
     }
-  }
-  let countArr = [];
-  function toggleHides(){
+}
+let countArr = [];
+
+function toggleHides() {
     toggleHide()
     toggleHide()
 }
- 
- 
 
 
 
-    
- 
- 
+
+
+// let feature = "https://script.google.com/macros/s/AKfycbxa6iiG3ohmI6ZaEmBcp8vGzRmM61MN542Lpe0fmnVjIise9VTrBQECY8oPjlk-RGSZ/exec"
+
+
+
+// //Loading products from JSON to our Shop page
+// function loadJSON() {
+//     let feature = "https://script.google.com/macros/s/AKfycbzD8GcgXkv9CzyjSIduuaakwFOZWkU-V4d6iw7UFvl25QmHxeL_MQnxOC7N5pEQKkIr/exec"
+//     fetch(`${feature}`)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw Error("ERROR");
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         // console.log(featured)
+//         // console.log(data[0]);
+
+//         //featured
+//         const html = data.map((f, i) => {
+//             return `
+//             <div class="product text-center col-lg-3 col-md-6 col-12">
+//             <img class="img-fluid mb-3 p-img" src="${data[i].Image}" alt="">
+//             <div class="star">
+//               <i class='bx bxs-star'></i>
+//               <i class='bx bxs-star'></i>
+//               <i class='bx bxs-star'></i>
+//               <i class='bx bxs-star'></i>
+//               <i class='bx bxs-star'></i>
+//             </div>
+//             <h5 class="p-name">${data[i].Name}</h5>
+//             <h4 class="p-price">₹${data[i].Price}</h4>
+//             <h4 class="p-price" style="display: none;">${data[i].Tags}</h4>
+//             <button class="buy-button" onclick ="msg()">Add to Cart</button>
+
+//           </div>`;
+//         }).join('');
+//         document.querySelector("#all").insertAdjacentHTML("afterbegin", html);
+//         // document.getElementById('all').innerHTML = html;
+//     })
+// }
--- healer/featured.js
+++ healer/featured.js
@@ -1,24 +1,17 @@
 let feature = "https://script.google.com/macros/s/AKfycbw1uZq2LRy6YrlmKI8xPWS39180cJC8oZWTAP2ieIvP4mjOLElyEZYIAs0X_nVbpnM/exec"
 
-
-fetch(`${feature}`)
-    .then(response => {
-        if (!response.ok) {
-            throw Error("ERROR");
-        }
-        return response.json();
-    })
-    .then(data => {
-        // console.log(data)
+;(async ()=>{
+    try {
+        let data = await (await fetch(`${feature}`)).json();
         let featured = data.filter(o => o.Type === 'featured');
         let oversize = data.filter(o => o.Type === 'oversize');
         let baggie = data.filter(o => o.Type === 'baggy');
         let hoodie = data.filter(o => o.Type === 'hoodie');
-        // console.log(featured)
-        // console.log(data[0]);
+
         featured = featured.slice(0,8)
         baggie = baggie.slice(0,4)
-        // console.log(baggie)
+        hoodie = hoodie.slice(0,4)
+
         //featured
         const featured_html = featured.map((f, i) => {
             return `
@@ -34,11 +27,35 @@ fetch(`${feature}`)
         <h5 class="p-name">${(featured[i].Name)}</h5>
         <h4 class="p-price">₹${featured[i].Price}</h4>
         <h4 class="p-price" style="display: none;">${featured[i].Tags}</h4>
-        <a href="shop.html"><button class="buy-button">Shop Now</button></a>
+        <a href="shop.html?filter=oversize&product=${i+1}"><button class="buy-button">Shop Now</button></a>
 
         </div>`;
         }).join('');
         document.querySelector("#featuredl").insertAdjacentHTML("afterbegin", featured_html);
+
+        //accessories
+        const accessories_html = accessories.map((f, i) => {
+            return `
+            <div class="product text-center col-lg-3 col-md-6 col-12">
+            <img src="${accessories[i].Image}" width="auto" height="217px" alt="">
+            <div class="star">
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+              <i class='bx bxs-star'></i>
+            </div>
+            <h5 class="p-name">${accessories[i].Name}</h5>
+            <h4 class="p-price">₹${accessories[i].Price}</h4>
+            <h4 class="p-price" style="display: none;">${accessories[i].Tags}</h4>
+            <a href="shop.html?filter=accessories&product=${i+1}"><button class="buy-button">Shop Now</button></a>
+    
+          </div>
+
+        </div>`;
+        }).join('');
+        document.querySelector("#accessories").insertAdjacentHTML("afterbegin", accessories_html);
+
 
         //baggie
         const baggy_html = baggie.map((f, i) => {
@@ -55,34 +72,17 @@ fetch(`${feature}`)
             <h5 class="p-name">${baggie[i].Name}</h5>
             <h4 class="p-price">₹${baggie[i].Price}</h4>
             <h4 class="p-price" style="display: none;">${baggie[i].Tags}</h4>
-            <a href="shop.html"><button class="buy-button">Shop Now</button></a>
+            <a href="shop.html?filter=baggy&product=${i+1}"><button class="buy-button">Shop Now</button></a>
     
           </div>
 
         </div>`;
         }).join('');
         document.querySelector("#baggy").insertAdjacentHTML("afterbegin", baggy_html);
+        
 
-        // //skins
-        // const skins_html = skins.map((f, i) => {
-        //     return `
-        //     <div class="product text-center col-lg-3 col-md-6 col-12">
-        //     <img class="img-fluid mb-3" src="${skins[i].Image}" alt="">
-        //     <div class="star">
-        //       <i class='bx bxs-star'></i>
-        //       <i class='bx bxs-star'></i>
-        //       <i class='bx bxs-star'></i>
-        //       <i class='bx bxs-star'></i>
-        //       <i class='bx bxs-star'></i>
-        //     </div>
-        //     <h5 class="p-name">${skins[i].Name}</h5>
-        //     <h4 class="p-price">₹${skins[i].Price}</h4>
-        //     <h4 class="p-price" style="display: none;">${skins[i].Tags}</h4>
-        //     <a href="shop.html"><button class="buy-button">Shop Now</button></a>
+    } catch (error) {
+        console.log("River",error)
+    }
     
-        //   </div>
-
-        // </div>`;
-        // }).join('');
-        // document.querySelector("#skins").insertAdjacentHTML("afterbegin", skins_html);
-    })
+})();
